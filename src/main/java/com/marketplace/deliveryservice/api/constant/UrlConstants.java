53c8a6598408de422fdff46ee20dfb0d02bfb13a
admin <maksimova2800@gmail.com>
2023-02-16 22:03:32 +0300

    init

---
+++ src/main/java/com/marketplace/deliveryservice/api/constant/UrlConstants.java
@@ -0,0 +1,13 @@
+package com.marketplace.deliveryservice.api.constant;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Класс содержит константы для формирования url.
+ *
+ */
+@UtilityClass
+public class UrlConstants {
+
+    public static final String MAIN_URL = "/example";
+}
---
+++ src/main/java/com/marketplace/deliveryservice/api/dto/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.marketplace.deliveryservice.api.dto;
+
+/**
+ * ErrorCode.
+ *
+ */
+public enum ErrorCode {
+    VALIDATION_ERROR
+}
